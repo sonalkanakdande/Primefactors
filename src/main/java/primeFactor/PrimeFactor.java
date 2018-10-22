94a451d645577ab70c051032e36b7768c5163cb1
sonalkanakdande <sonalkanakdande@users.noreply.github.com>
2018-10-22 17:31:03 +0800

    Final Code

--- src/main/java/primeFactor/PrimeFactor.java
+++ src/main/java/primeFactor/PrimeFactor.java
@@ -7,14 +7,12 @@ import java.util.List;
 public class PrimeFactor {
 
     public static List<Integer> Of(int n) {
-        List primeFactors = Lists.newArrayList() ;
-        if (n>1){
-            if (n%2==0){
-                n/=2;
-                primeFactors.add(2);
+        List primeFactors = Lists.newArrayList();
+        for (int divisor = 2; n > 1; divisor++) {
+            for (; n % divisor == 0; n /= divisor) {
+                primeFactors.add(divisor);
             }
         }
         return primeFactors;
     }
-
 }
--- src/test/java/primeFactor/PrimeFactorTest.java
+++ src/test/java/primeFactor/PrimeFactorTest.java
@@ -19,6 +19,17 @@ public class PrimeFactorTest {
 
     @Test
     public void primeFactorOfThree() {
-        Assert.assertEquals(PrimeFactor.Of(2), ImmutableList.<Integer>of(2) );
+        Assert.assertEquals(PrimeFactor.Of(3), ImmutableList.<Integer>of(3) );
     }
+
+    @Test
+    public void primeFactorOfFour() {
+        Assert.assertEquals(PrimeFactor.Of(4), ImmutableList.<Integer>of(2,2) );
+    }
+
+    @Test
+    public void primeFactorOfSix() {
+        Assert.assertEquals(PrimeFactor.Of(6), ImmutableList.<Integer>of(2,3) );
+    }
+
 }
